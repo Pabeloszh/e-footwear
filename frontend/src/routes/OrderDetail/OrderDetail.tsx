50c7e93b7b52b87a6793831ee68cebbc809fe4a8
Pabeloszh <Pabeloszh@gmail.com>
2022-01-27 23:05:38 +0100

    order detail fixed: divided to separate components

--- frontend/src/routes/OrderDetail/OrderDetail.tsx
+++ frontend/src/routes/OrderDetail/OrderDetail.tsx
@@ -1,58 +1,13 @@
-import { useHistory } from 'react-router-dom';
-import * as yup from 'yup';
-import { useFormik } from 'formik';
-import { TextField } from '@material-ui/core';
-import Button from '@mui/material/Button';
-import { useParams } from 'react-router';
-import { ProfileHistory } from '../../components/Profile/ProfileHistory';
+import { OrderSearch } from '../../components/OrderDetail/OrderSearch';
+import { OrderData } from '../../components/OrderDetail/OrderData';
 import { StyledOrderDetail } from './OrderDetail.style';
 
-const validationSchema = yup.object({
-    orderId: yup
-        .string()
-        .required('Order id is required'),
-});
 
 export const OrderDetail = () => {
-    let { id } = useParams() as {
-        id: string;
-    }
-    
-    const history = useHistory()
-
-    const formik = useFormik({
-        initialValues: {
-            orderId: id,
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            history.push(`/order-detail/${values.orderId}`);
-        },
-    });
-    
     return (
         <StyledOrderDetail>
-            <form onSubmit={formik.handleSubmit}>
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    id="orderId"
-                    label="Search by order id"
-                    name="orderId"
-                    autoComplete="orderId"
-                    value={formik.values.orderId}
-                    onChange={formik.handleChange}
-                    error={formik.touched.orderId && Boolean(formik.errors.orderId)}
-                    helperText={formik.touched.orderId && formik.errors.orderId}
-                />
-                <Button 
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                >Search</Button>
-            </form>
-            <ProfileHistory/>
+            <OrderSearch/>
+            <OrderData/>
         </StyledOrderDetail>
     );
 };
